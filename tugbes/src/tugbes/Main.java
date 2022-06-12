69ce32cf577c811236bd45cc64c475c5015c7ef2
AniLutfiyah <101568393+AniLutfiyah@users.noreply.github.com>
2022-06-12 22:14:08 +0700

    Add files via upload
    Aplikasi Random Nama
    Nama : Ani Lutfiyah
    NIM : 32602000071

---
+++ tugbes/src/tugbes/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugbes;
+
+import tugbes.aplikasirandom;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args){
+        aplikasirandom wu = new aplikasirandom();
+        wu.setVisible(true);
+    }
+}
